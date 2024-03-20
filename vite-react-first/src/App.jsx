e86a59a47d8e5fce6f0e3ea82c35fdf37e3073f5
njy622 <njy622@naver.com>
2024-03-20 21:55:44 +0900

    20240320_4차시 props

--- vite-react-first/src/App.jsx
+++ vite-react-first/src/App.jsx
@@ -1,19 +1,12 @@
-import Avatar from './components/Avatar'
+import Profile from './components/Profile';
 
 
 export default function App() {
 
-  const greet = 'Hello world';
-  const footerMessage = 'Copyright 2023 by Nam'
   return (
-    <> 
-     {/* <h1>Hello world</h1> static한 데이터*/}
-      <h1>{greet}</h1>
-      <hr /> 
-      <Avatar/>
-      {/*<h3>Copyright 2023 by Nam</h3>  static한 데이터*/}
-      <h3>{footerMessage}</h3>
-    </>
+    <>
+      <Profile/>
+    </> 
   );
 }
 
---
+++ vite-react-first/src/components/Card.jsx
@@ -0,0 +1,10 @@
+export default function Card({children}) {
+    return(
+     <div className="card">
+        {children}
+     </div>
+    );
+}
+
+// children을 이용한 템플릿 사용  Vue에서는 슬롯
+
