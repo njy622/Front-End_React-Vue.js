d036142f3118e63a7511719aaa30a7db5910a255
njy622 <njy622@naver.com>
2024-03-29 11:29:16 +0900

    20240329_19차시. 리액트 최종 프로젝트

--- react-movie-app/src/App.jsx
+++ react-movie-app/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MovieFooter from "./components/MovieFooter"
 import MovieHeader from "./components/MovieHeader"
 import MovieList from "./components/MovieList"
@@ -5,12 +6,35 @@ import MovieList from "./components/MovieList"
 
 function App() {
 
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=> {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzVmMGNlMmMxZGU5MjFkMDMzNTYzYTJkMDlkNWVhYiIsInN1YiI6IjY2MDYxYzg2MjgzZWQ5MDE2MjE3ZWU4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sBsJMoKbymovrY1LwP4SRTsQ3nnRd-hH8sx5s0RKoRM'
+      }
+    };
+    
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=ko&page=1', options)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response.results);
+        setMovies(response.results);
+      })
+      .catch(err => console.error(err));
+
+  },[]);
   
+   const handleMovies = (id) => {
+      console.log(id);
+   }
 
   return (
     <>
-      <MovieHeader/>
-      <MovieList/>
+      <MovieHeader onMobieData={handleMovies}/>
+      <MovieList movies={movies}/>
       <MovieFooter/>
 
     </>
--- react-movie-app/src/main.jsx
+++ react-movie-app/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+{/* <React.StrictMode> React.StrictMode 개발시 디버깅을 위한 것 */}
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>,
 )
